bbfbb1642af38ba8b790f3a210e7b5640ed91f56
Artur Oscar <Arturoscar3@gmail.com>
2022-02-01 19:41:20 -0300

    Solve bugs

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,12 @@ import Header from './components/Header';
 function App() {
   return (
     <Router>
-      <Header>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/sobre' element={<Sobre />} />
-
-          <Route path='*' element={<Pagina404 />} />
-        </Routes>
-      </Header>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route element={<Pagina404 />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import imagem from '../assets/img/doguito.svg';
 import '../assets/css/componentes/cabecalho.css';
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className='cabecalho container'>
@@ -8,34 +9,34 @@ const Header = () => {
         <span className='menu-hamburguer__icone'></span>
       </div>
       <div className='cabecalho-container'>
-        <a href='/' className='flex flex--centro'>
+        <Link to='/' className='flex flex--centro'>
           <img className='cabecalho__logo' src={imagem} alt='Logo Doguito' />
           <h1 className='cabecalho__titulo'>PetShop</h1>
-        </a>
+        </Link>
       </div>
       <nav className='menu-cabecalho'>
-        <uL className='menu-itens'>
+        <ul className='menu-itens'>
           <li>
-            <a href='#' className='menu-item menu-item--entrar'>
+            <Link to='#' className='menu-item menu-item--entrar'>
               Entrar
-            </a>
+            </Link>
           </li>
           <li>
-            <a href='#' className='menu-item'>
+            <Link to='#' className='menu-item'>
               Produtos
-            </a>
+            </Link>
           </li>
           <li>
-            <a href='/' className='menu-item'>
+            <Link to='/' className='menu-item'>
               Blog
-            </a>
+            </Link>
           </li>
           <li>
-            <a href='/sobre' className='menu-item'>
+            <Link to='/sobre' className='menu-item'>
               Sobre
-            </a>
+            </Link>
           </li>
-        </uL>
+        </ul>
       </nav>
       <div className='menu-cabecalho-background'></div>
     </header>
